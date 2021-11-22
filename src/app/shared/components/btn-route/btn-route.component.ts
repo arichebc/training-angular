3542795d0ca26d0e294dd73d03da6c252977107d
christophe <christophegueroult@gmail.com>
2021-11-22 15:16:05 +0100

    all stupid components

---
+++ src/app/shared/components/btn-route/btn-route.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-route',
+  templateUrl: './btn-route.component.html',
+  styleUrls: ['./btn-route.component.scss']
+})
+export class BtnRouteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TabLightComponent } from './components/tab-light/tab-light.component';
+import { BtnRouteComponent } from './components/btn-route/btn-route.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TabLightComponent,
+    BtnRouteComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TabLightComponent,
+    BtnRouteComponent
   ]
 })
 export class SharedModule { }
